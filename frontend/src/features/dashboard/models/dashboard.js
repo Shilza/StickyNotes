3edb4944832c053146f279125e368d6529c889a7
Galatinov <andgalatinov@gmail.com>
2019-06-16 00:25:43 +0300

    Some fixes

--- frontend/src/features/dashboard/models/dashboard.js
+++ frontend/src/features/dashboard/models/dashboard.js
@@ -6,6 +6,7 @@ export const addRecord = createEvent('addRecord');
 export const addColumn = createEvent('addColumn');
 export const removeColumn = createEvent('removeColumn');
 export const reorderRecords = createEvent('reorderRecords');
+export const updateRecord = createEvent('updateRecord');
 export const resetDashboard = createEvent('resetDashboard');
 
 const initialState = {
@@ -15,27 +16,43 @@ const initialState = {
 export const $dashboard = createStore(initialState)
     .on(setColumns, (state, columns) => {
         state.columns = columns;
+        return {...state};
     })
     .on(addRecord, (state, {columnId, record}) => {
-        state.columns.forEach((column, index) => {
+        state.columns.map((column, index) => {
             if(column.id === columnId)
                 state.columns[index].records.push(record);
-        })
+
+            return column;
+        });
+        return {...state};
     })
     .on(addColumn, (state, column) => {
+        if(!Array.isArray(column.records))
+            column.records = [];
         state.columns.push(column);
+
+        return {...state};
     })
     .on(reorderRecords, (state, {columnId, oldIndex, newIndex}) => {
+        state.columns.map(column => {
+            if(column.id === columnId)
+                column.records = arrayMove(column.records, oldIndex, newIndex);
+            return column;
+        });
+        return {...state};
+    })
+    .on(updateRecord, (state, {recordId, text}) => {
         state.columns.forEach(column => {
-            if(column.id === columnId) {
-                column.records = arrayMove(column.records, oldIndex, newIndex)
-            }
-        })
+            column.records.forEach(record => {
+                if(record.id === recordId)
+                    record.text = text;
+            })
+        });
+        return {...state};
     })
     .on(removeColumn, (state, columnId) => {
-        state.columns = state.columns.map(column => {
-            if(column.id !== columnId)
-                return column;
-        });
+        state.columns = state.columns.filter(column => column.id !== columnId);
+        return {...state};
     })
     .reset(resetDashboard);
--- frontend/src/features/dashboard/selectors/index.js
+++
@@ -1,4 +0,0 @@
-
-export const getRecordsByColumnId = (state, id) => (
-    state.columns.find(column => column.id === id).records
-);
