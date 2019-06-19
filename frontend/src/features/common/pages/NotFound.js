27763c39f7c2c7318e426b242e0343f39f1d1f99
Galatinov <andgalatinov@gmail.com>
2019-06-20 01:09:36 +0300

    Add NotFound page

---
+++ frontend/src/features/common/pages/NotFound.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+export const NotFound = () => (
+    <Container>
+        Page not found
+    </Container>
+);
