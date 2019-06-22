c3a05e71a6509da23b264e49c1583b7f3569dae6
Galatinov <andgalatinov@gmail.com>
2019-06-22 14:20:26 +0300

    Unregister service worker

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d8a5f7d2856f505904783ab82f0becb5.js"
+  "/precache-manifest.19925917fcc0eb7b7e42a14d92540ff0.js"
 );
 
 self.addEventListener('message', (event) => {
