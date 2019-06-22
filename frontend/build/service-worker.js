5bc8489b38b10bfb48bb50698a326a3b3f681b6b
Galatinov <andgalatinov@gmail.com>
2019-06-22 22:30:39 +0300

    Build frontend

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19925917fcc0eb7b7e42a14d92540ff0.js"
+  "/precache-manifest.0abc8e22a8284ac43871d011f92929a9.js"
 );
 
 self.addEventListener('message', (event) => {
